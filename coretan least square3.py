9dbd41b9452817710e8247c886c03dfcc84cb813
Muhammad Husni <52745045+muhammadhusni777@users.noreply.github.com>
2024-05-20 21:17:45 +0700

    Add files via upload

--- coretan least square3.py
+++ coretan least square3.py
@@ -10,13 +10,14 @@ b0 = 3
 b1 = 0
 
 t_memory = []
-a_memory = []#np.array([0])
+a_memory = []
 b_memory = []
 
-theta_hat = [b0, b1, -a1]#np.zeros((3, 1))  
+theta_hat = [b0, b1, -a1]
 
 P = 10000 * np.eye(3)  # Matriks kovariansi awal
-N = 1000  # Jumlah iterasi
+print(P)
+N = 100  # Jumlah iterasi
 noise_variance = 0.8**2  # Varians noise
 
 # Input sistem
@@ -28,6 +29,9 @@ y = np.zeros(N)
 
 # Simulasi RLS dengan input impuls
 for t in range(1, N):
+    if (t==1):
+        print(theta_hat)
+    
     phi = np.array([[u[t]], [u[t-1]], [y[t-1]]])  # Vektor regressor
     K = P.dot(phi) / (lambda_ + phi.T.dot(P).dot(phi))  # Gain RLS
     e_t = np.random.normal(0, np.sqrt(noise_variance))  # White noise
@@ -35,23 +39,22 @@ for t in range(1, N):
     #print(e_t)
     y[t] = -1.5 * y[t-1] + 2 * u[t-1] + e_t  # Update output dengan white noise
     theta_hat = theta_hat + K * (y[t] - phi.T.dot(theta_hat))  # Update estimasi parameter
-    print(theta_hat[1][1])
+    #print(theta_hat[1][1])
     #a_memory = np.append(a_memory, theta_hat[2], axis = 0)
     
     P = (P - K.dot(phi.T).dot(P)) / lambda_  # Update matriks kovariansi
 
     t_memory.append(t)
-    a_memory.append(theta_hat[1][1])
+    a_memory.append(theta_hat[1][2])
     b_memory.append(theta_hat[2][0])  
 
 # Hasil estimasi parameter
+'''
 print("Estimasi parameter setelah 100 iterasi:")
 print(theta_hat)
-
 print(a_memory)
-
 print(t)
-
+'''
 
 # Plot the fitted line
 plt.plot(t_memory, a_memory, color='red', label='a')
